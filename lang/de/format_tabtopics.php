7c553221c81e96bc03a60ac9a51b804cd53ef283
Johannes Burk <j.burk@joois.net>
2014-09-30 13:23:12 +0200

    german language strings for vertical tabtopics option added

---
+++ lang/de/format_tabtopics.php
@@ -0,0 +1,4 @@
+<?php
+
+$string['tabtopics_tabdisplay_vertical'] = 'Vertikale Tabs';
+$string['tabtopics_tabdisplay_vertical_help'] = 'Wenn aktiviert, werden die Tabs vertikal, untereinander auf der linken Seite angezeigt anstatt oberhalb in einer Zeile nebeneinander.';
