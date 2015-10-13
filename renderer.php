jojoob/moodle-format_tabtopics

commit fa5ab5f67d188ee47845e78f1391ce2a30d8e57a

method signature adjusted/corrected

renderer.php
@@ -108,8 +108,8 @@ class format_tabtopics_renderer extends format_section_renderer_base
      * 
      * @param type $section
      */
-    public function section_hidden($section) {
-        echo parent::section_hidden($section);
+    public function section_hidden($sectionno, $courseorid = NULL) {
+        echo parent::section_hidden($section, $courseorid);
     }
     
 }

commit 56f97f1a26c214262cb329f5e8682cfc7a10e89c

Fix: wrong variable name (caused by fa5ab5f67d188ee47845e78f1391ce2a30d8e57a)

renderer.php
@@ -109,7 +109,7 @@ class format_tabtopics_renderer extends format_section_renderer_base
      * @param type $section
      */
     public function section_hidden($sectionno, $courseorid = NULL) {
-        echo parent::section_hidden($section, $courseorid);
+        echo parent::section_hidden($sectionno, $courseorid);
     }
     
 }