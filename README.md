424ab57ca12003a569b4b0b64b9bc426b135e10e
Johannes Burk <me@jojoob.de>
2015-10-28 08:44:15 +0100

    version and README adjusted (fork)

---
+++ README.md
@@ -0,0 +1,14 @@
+moodle-format_tabtopics
+=======================
+
+**Plugin forked from [oohoo/moodle-format_tabtopics](https://github.com/oohoo/moodle-format_tabtopics), thanks to [oohoo](https://github.com/oohoo/).**
+
+This is a Moodle course format plugin (https://docs.moodle.org/dev/Course_formats) that displays course sections as tabs.
+
+## Installation
+1. cd moodle/course/format/
+2. git clone https://github.com/sudile/moodle-format_tabtopics tabtopics
+3. Go to your moodle.site/admin to finish installation
+
+## Compatibility
+This plugin version was tested and is compatible with Moodle versions 2.5+, 2.6+, 2.7+, 2.8+, 2.9+.
--- version.php
+++ version.php
@@ -1,26 +1,32 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * *************************************************************************
- * *                 OOHOO Tab topics Course format                       **
- * *************************************************************************
- * @package     format                                                    **
- * @subpackage  tabtopics                                                 **
- * @name        tabtopics                                                 **
- * @copyright   oohoo.biz                                                 **
- * @link        http://oohoo.biz                                          **
- * @author      Nicolas Bretin                                            **
- * @author      Braedan Jongerius                                         **
- * @author      Dustin Durand                                             **
- * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later  **
- * *************************************************************************
- * ************************************************************************ */
-
+ * Version details
+ *
+ * @package    format_tabtopics
+ * @copyright  oohoo.biz
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ * @author     Nicolas Bretin, Braedan Jongerius, Dustin Durand, Johannes Burk <me@jojoob.de>
+ */
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014103000;        // The current plugin version (Date: YYYYMMDDXX).
-$plugin->requires = 2013051400;        // Requires this Moodle version.
-$plugin->component = 'format_tabtopics';    // Full name of the plugin (used for diagnostics).
+$plugin->version = 2015102800;
+$plugin->requires = 2013051400; // moodle 2.5.0
+$plugin->component = 'format_tabtopics';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.6.0 (Build: 2014040200)';
+$plugin->release = 'fork/2.7.0 (Build: 2015102800)';
