085e8f084e2f549876aed5183203adf2d546f4b8
Johannes Burk <me@jojoob.de>
2016-02-09 11:44:56 +0100

    Fix: course displayed empty on first view (if no sections existing)
    fixes #3

--- format.php
+++ format.php
@@ -24,6 +24,8 @@ $PAGE->requires->js('/course/format/tabtopics/module.js');
 // make sure all sections are created
 $course = course_get_format($course)->get_course();
 course_create_sections_if_missing($course, range(0, $course->numsections));
+$modinfo = get_fast_modinfo($course);
+$sections = $modinfo->get_section_info_all();
 //Replace get_context_instance by the class for moodle 2.6+
 if(class_exists('context_module'))
 {
